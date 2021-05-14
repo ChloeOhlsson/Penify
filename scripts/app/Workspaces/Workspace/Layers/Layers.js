5559b0822e814d4029be4c866e99ab6814a5b99e
Cake <78360666+CakeA@users.noreply.github.com>
2021-05-14 10:30:45 +0200

    Added dialog overlays and layer deletion

--- scripts/app/Workspaces/Workspace/Layers/Layers.js
+++ scripts/app/Workspaces/Workspace/Layers/Layers.js
@@ -30,10 +30,49 @@ class Layers extends WorkspaceItem {
             <div class="layer-name">${layer.name}</div>
 
             <div class="layer-buttons">
-                <button><i class="fas fa-times"></i></button>
+                <button class="layer-remove"><i class="fas fa-times"></i></button>
             </div>
         `;
+
+        layer.element.querySelector(".layer-remove").addEventListener("click", (event) => {
+            this.remove(layer);
+        });
         
         this.container.appendChild(layer.element);
+    };
+
+    remove(layer) {
+        const element = document.createElement("div");
+        element.classList.add("dialog-overlay");
+        element.innerHTML = `
+            <div class="dialog-overlay-content">
+                Are you sure you want to delete ${layer.name}?
+
+                <div class="dialog-overlay-buttons">
+                    <button class="fill remove">Remove</button>
+
+                    <button class="fill-transparent cancel">Cancel</button>
+                </div>
+            </div>
+        `;
+
+        element.addEventListener("click", (event) => {
+            if(event.target != element)
+                return;
+
+            element.remove();
+        });
+
+        element.querySelector(".cancel").addEventListener("click", (event) => {
+            element.remove();
+        });
+
+        element.querySelector(".remove").addEventListener("click", (event) => {
+            element.remove();
+            
+            layer.element.remove();
+        });
+
+        this.element.appendChild(element);
     };
 };
